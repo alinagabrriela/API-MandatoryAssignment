5ab3d2ffc76725751dfda15c04811baa207efddb
alinaagab <alin1202@edu.easj.dk>
2021-04-23 10:34:03 +0200

    GetRoom-display working

--- Indoor-Climate-App-Mandatory-Assignment--FinalVersion/PROJECT ROOT/Indoor-Climate-App/src/app/api-popup/api-popup.page.ts
+++ Indoor-Climate-App-Mandatory-Assignment--FinalVersion/PROJECT ROOT/Indoor-Climate-App/src/app/api-popup/api-popup.page.ts
@@ -1,32 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+
 import {Observable} from 'rxjs';
+
+
 
 import {HttpClient} from '@angular/common/http';
 
-@Component({
-  selector: 'app-api-popup',
-  templateUrl: './api-popup.page.html',
-  styleUrls: ['./api-popup.page.scss'],
-})
-export class ApiPopupPage implements OnInit {
-  public rooms: Observable<any>;
- 
 
-  constructor(public httpClient: HttpClient) { 
-    this.rooms = this.httpClient.get('http://localhost:3000/room');
-    this.rooms
-    .subscribe(data => {
-      this.rooms = data.results;
-        resolve(this.rooms);
-    })
-   
-  }
+
+@Component({
+
+  selector: 'app-api-popup',
+
+  templateUrl: './api-popup.page.html',
+
+  styleUrls: ['./api-popup.page.scss'],
+
+})
+
+
+
+export class ApiPopupPage implements OnInit {
+
+
+
+  rooms: Observable<any>;
+
+  public roomArray = [];
+
+
+
+    constructor(public httpClient: HttpClient) {
+
+
+
+      this.rooms = this.httpClient.get('http://localhost:3000/room');
+
+      this.rooms
+
+      .subscribe(data => {
+
+        console.log('Data in Room Observable object: ', data);
+
+        this.roomArray = data;
+
+        console.log('Data in Room array: ', this.roomArray);
+
+
+
+      })
+
+
+
+    }
+
+
 
   ngOnInit() {
+
   }
 
 }
-function resolve(rooms: Observable<any>) {
-  throw new Error('Function not implemented.');
-}
+
+
+
+
+
+
+
 
